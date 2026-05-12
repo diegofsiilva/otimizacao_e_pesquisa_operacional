881e5d49e91c5a916a3f60472be23e3e671e4a3c
richard.alves <richard.alves@sou.inteli.edu.br>
2026-05-12 01:33:58 -0300

    feat: implement construir_tableau_inicial

--- apps/algoritmo_simplex/simplex.py
+++ apps/algoritmo_simplex/simplex.py
@@ -5,6 +5,7 @@ Implementação do algoritmo Simplex para problemas de programação linear.
 
 from models import Problema, Tableau
 
+
 def construir_tableau_inicial(problema: Problema) -> Tableau:
     """
     Constrói o tableau inicial a partir de um problema de programação linear.
@@ -16,7 +17,38 @@ def construir_tableau_inicial(problema: Problema) -> Tableau:
     Retorna:
         Tableau inicial pronto para o algoritmo Simplex
     """
-    pass
+    n = len(problema.c)  # número de variáveis de decisão
+    m = len(problema.b)  # número de restrições
+
+    valores_iniciais = problema.b
+    indices_variaveis_folga = list(range(n, n + m))
+    contribuicao_variaveis_folga = [0.0] * m
+
+    x = []
+
+    for i in range(n):
+        coluna = []
+
+        for j in range(m):
+            coluna.append(problema.A[j][i])
+
+        x.append(coluna)
+
+    s = []
+
+    for i in range(m):
+        coluna = [0.0] * m
+        coluna[i] = 1.0
+        s.append(coluna)
+
+    return Tableau(
+        contributions=contribuicao_variaveis_folga,
+        base=indices_variaveis_folga,
+        values=valores_iniciais,
+        x=x,
+        s=s,
+    )
+
 
 def simplex(problema: Problema) -> tuple[list[float], float]:
     """
@@ -29,4 +61,6 @@ def simplex(problema: Problema) -> tuple[list[float], float]:
         x: instância de Problema contendo c, A e b (tamanho N)
         z: valor ótimo da função objetivo
     """
+    n = len(problema.c)  # número de variáveis de decisão
+    m = len(problema.b)  # número de restrições
     pass
